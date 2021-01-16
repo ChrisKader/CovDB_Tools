8a73e27835e410607ca9f3595c6e7dbd91202eb1
Chris Kader <chris.kader@gmail.com>
2021-01-16 17:28:58 -0600

    Changes to upload.sh. Anticipate change to PS.

--- upload.sh
+++ upload.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-cd ./CovDB
-TAG=$(grep Version CovDB.toc | cut -d ':' -f2 | tr -d ' ')
+cd ./CovDB_Addon
+TAG=$(grep Version ./CovDB_US_H/CovDB_US_H.toc | cut -d ':' -f2 | tr -d ' ' )
 git commit -am "Update database $TAG"
 git tag $TAG
-git push origin master
+git push origin main
 git push origin $TAG
 cd ..
-zip -r CovDB-$TAG.zip CovDB/*
+zip -r ./addon_dist/CovDB-$TAG.zip CovDB_Addon/*
 
 # https://authors.curseforge.com/knowledge-base/projects/529-api
-echo curl -XPOST -d "{changelog: \"A string describing changes.\", displayName: \"$TAG\", gameVersions: [902], releaseType: \"release\" }"
+#echo curl -XPOST -d "{changelog: \"A string describing changes.\", displayName: \"$TAG\", gameVersions: [902], releaseType: \"release\" }"
